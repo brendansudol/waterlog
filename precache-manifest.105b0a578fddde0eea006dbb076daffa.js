255ecf185b1798614d47e571d89127b5a9c81bae
brendan sudol <brendansudol@gmail.com>
2019-05-13 08:42:28 -0400

    Updates

---
+++ precache-manifest.105b0a578fddde0eea006dbb076daffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea3b9e8e98ce37ff40494f2dbb4f794",
+    "url": "/waterlog/index.html"
+  },
+  {
+    "revision": "ded5005264d25922b5e6",
+    "url": "/waterlog/static/css/main.a0c7f5d5.chunk.css"
+  },
+  {
+    "revision": "2d3374334b879fc9b8a7",
+    "url": "/waterlog/static/js/2.2e9823cd.chunk.js"
+  },
+  {
+    "revision": "ded5005264d25922b5e6",
+    "url": "/waterlog/static/js/main.d7423639.chunk.js"
+  },
+  {
+    "revision": "4393ef059eb2cf1d87db",
+    "url": "/waterlog/static/js/runtime~main.32112e4c.js"
+  }
+]);
